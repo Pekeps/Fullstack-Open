758499d32b709415932defa011b38ee160f9b6df
Pekko Pesonen <pekko.koulu@gmail.com>
2023-05-14 20:31:14 +0300

    t1.12-1.14

--- Osa1/anekdootit/src/App.js
+++ Osa1/anekdootit/src/App.js
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const Button = (props) => {
+  return (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+  )
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -11,12 +19,34 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+
+  const [points, setPoints] = useState({0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0})
   const [selected, setSelected] = useState(0)
+
+  const addVote = () => {
+    const copy = {...points}
+    copy[selected] += 1
+    setPoints(copy)
+    console.log(points)
+  }
+
+  const anecdoteWithMostVotes = Object.entries(points).reduce((a,points) => a[1] > points[1] ? a : points)[0]
+  console.log(anecdoteWithMostVotes)
+  
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
+      <br></br>
+      has {points[selected]} votes
+      <br></br>
+      <Button handleClick={() => setSelected(Math.floor(Math.random() * 8))} text='next anecdote' />
+      <Button handleClick={() => addVote()} 
+      text='add vote' />
+
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[anecdoteWithMostVotes]}
     </div>
   )
 }
