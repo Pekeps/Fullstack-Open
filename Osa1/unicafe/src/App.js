fc41fce5bd2f524be25a23ddbc78160e84d8c2f4
Pekeps <118191015+Pekeps@users.noreply.github.com>
2023-05-15 09:55:13 +0300

    Update App.js

--- Osa1/unicafe/src/App.js
+++ Osa1/unicafe/src/App.js
@@ -1,22 +1,70 @@
-import { useState } from 'react'
+mport { useState } from 'react'
+
+const StatisticLine = ({ text, value }) =>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+
+const Statistics = ({good, bad, neutral}) => {
+  if (good + bad + neutral === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+
+  return(
+    <table>
+      <tbody>
+       <StatisticLine text='good' value={good} />
+       <StatisticLine text='neutral' value={neutral} />
+       <StatisticLine text='bad' value={bad} />
+       <StatisticLine text='all' value={good + bad + neutral} />
+       <StatisticLine text='average' value={(good - bad) / (good + bad + neutral)}  />
+       <StatisticLine text='positive' value={good * 100 / (good + bad + neutral)} />    
+      </tbody>
+    </table>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
 
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
+  
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToGood = newValue => {
+    console.log('good reviews', newValue)
+    setGood(newValue)
+  }
+  const setToNeutral = newValue => {
+    console.log('neutral reviews', newValue)
+    setNeutral(newValue)
+  }
+  const setToBad = newValue => {
+    console.log('bad reviews', newValue)
+    setBad(newValue)
+  }
 
   return (
     <div>
-      {anecdotes[selected]}
+      <h1>give feedback</h1>
+      <Button handleClick={() => setToGood(good + 1)} text='good' />
+      <Button handleClick={() => setToNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setToBad(bad + 1)} text='bad' />
+
+      <h1>statistics</h1>
+      
+      <Statistics good={good} bad={bad} neutral={neutral} />
+      
     </div>
   )
 }
